d8a51362ada6700c408c9243cc2294286383f459
Reshmi <91744955+Reshmisivaraj@users.noreply.github.com>
2024-05-14 09:51:22 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,14 +8,14 @@ try:
 except Exception as e:
     st.error(f'Error loading model: {e}')
 
-def loan_status_prediction(no_of_dependents, education, self_employed, income_annum, loan_amount, loan_term, cibil_score, residential_assets_value, commercial_assets_value, luxury_assets_value, bank_asset_value):
+def loan_status_prediction(no_of_dependents, education, self_employed, income_annum, loan_amount, loan_term, cibil_score, residential_assets_value, commercial_assets_value, luxury_assets_value, bank_asset_value, threshold=0.85):
     input_data = [[no_of_dependents, education, self_employed, income_annum, loan_amount, loan_term, cibil_score, residential_assets_value, commercial_assets_value, luxury_assets_value, bank_asset_value]]
     predicted_loan_status = model.predict(input_data)[0]
-    return predicted_loan_status
+    return 1 if predicted_loan_status > threshold else 0
 
 def main():
     st.title('Loan Sanction Prediction')
-
+    threshold = 0.9 
     no_of_dependents = st.number_input('Enter Number of Dependents:', min_value=0, step=1)
     education = st.number_input('Enter Education (1: Graduate, 0: Not Graduate):', min_value=0, max_value=1, step=1)
     self_employed = st.number_input('Self Employed (1: Yes, 0: No):', min_value=0, max_value=1, step=1)
@@ -29,15 +29,12 @@ def main():
     bank_asset_value = st.number_input('Enter Bank Assets Value:', min_value=0, step=1000)
 
     if st.button('Predict Loan Sanction'):
-        predicted_status = loan_status_prediction(no_of_dependents, education, self_employed, income_annum, loan_amount, loan_term, cibil_score, residential_assets_value, commercial_assets_value, luxury_assets_value, bank_asset_value)
+        predicted_status = loan_status_prediction(no_of_dependents, education, self_employed, income_annum, loan_amount, loan_term, cibil_score, residential_assets_value, commercial_assets_value, luxury_assets_value, bank_asset_value, threshold)
         
-        if predicted_status >0.90 :
+        if predicted_status == 1:
             st.success('Loan will be sanctioned.')
         else:
             st.error('Loan will not be sanctioned.')
-        
 
 if __name__ == "__main__":
     main()
-
-
